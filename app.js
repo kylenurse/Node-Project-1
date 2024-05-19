9e9d5c841a7fd2701c960c0762260af13b40fd99
Kyle Nurse <kyle.nurse@ryerson.ca>
2024-05-18 20:19:14 -0400

    Became familiar with numerous express.js functions.

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const http = require('http');
 const bodyParser = require('body-parser');
 const express = require('express');
 
-const shopRoutes = require('./routes/shop')
-const adminRoutes = require('./routes/admin')
 
+const shopRoutes = require('./routes/shop')
+const adminRoutes = require('./routes/admin');
+const path = require('path');
 
 const app = express();
 app.get('/favicon.ico', (req, res) => res.status(204));
@@ -13,12 +14,12 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 
-app.use('/', (req,res,next) => {
-    res.send('<h1>Home Page</h1>');
+app.use((req,res,next) => {
+    res.status(404).sendFile(path.join(__dirname,  'views', 'notFound.html'));
 });
 
 app.listen(3000);
